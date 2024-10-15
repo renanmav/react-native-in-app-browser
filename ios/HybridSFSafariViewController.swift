6d62f4d02c7d6d55c4c0ea565efd9fe04ffa1706
Renan Mav <renan.mav@hotmail.com>
2024-10-14 21:47:02 -0300

    chore: create ios/HybridSFSafariViewController.swift

---
+++ ios/HybridSFSafariViewController.swift
@@ -0,0 +1,9 @@
+class HybridSFSafariViewController: HybridSFSafariViewControllerSpec {
+  func present(url: String) throws -> Void {
+    throw NSError(domain: "HybridSFSafariViewController", code: 0, userInfo: [NSLocalizedDescriptionKey: "Method not implemented"])
+  }
+
+  func dismiss() throws -> Void {
+    throw NSError(domain: "HybridSFSafariViewController", code: 0, userInfo: [NSLocalizedDescriptionKey: "Method not implemented"])
+  }
+}
